866f965cb74f3dfa18dd842d764b69be1b9a75a6
Raghavendra Kodavalla <raghavendrakodavalla@gmail.com>
2025-02-28 12:58:36 +0530

    deployment ready

---
+++ RMS_finalCut/frontend/main.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+ 
+let app = express();
+const staticPath = path.join(path.join(process.cwd(),'dist'));
+app.use(express.static(staticPath));
+app.get('*',(request,response)=>{
+    response.sendFile(path.join(path.join(process.cwd(),'dist')))
+})
+ 
+app.listen(80,()=>console.log("server is started on : http://localhost:80"));
