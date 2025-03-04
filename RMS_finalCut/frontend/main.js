6364463020dda0d5cb7848112223ba19045d11de
harshith <Kotha.Harshith@BRILLIO.COM>
2025-03-04 10:28:35 +0530

    monday

--- RMS_finalCut/frontend/main.js
+++ RMS_finalCut/frontend/main.js
@@ -8,4 +8,4 @@ app.get('*',(request,response)=>{
     response.sendFile(path.join(path.join(process.cwd(),'index.html')))
 })
  
-app.listen(80,()=>console.log("server is started on : http://localhost:80"));
+app.listen(80,()=>console.log("server is started on : http://54.163.10.39:80"));
