cb6082fa65fd8242bd382f572586d46c6c98d5e0
harshith <Kotha.Harshith@BRILLIO.COM>
2025-03-04 10:17:34 +0530

    deploy1

--- RMS_finalCut/frontend/main.js
+++ RMS_finalCut/frontend/main.js
@@ -5,7 +5,7 @@ let app = express();
 const staticPath = path.join(path.join(process.cwd(),'dist'));
 app.use(express.static(staticPath));
 app.get('*',(request,response)=>{
-    response.sendFile(path.join(path.join(process.cwd(),'dist')))
+    response.sendFile(path.join(path.join(process.cwd(),'index.html')))
 })
  
-app.listen(80,()=>console.log("server is started on : http://54.163.10.39:80"));
+app.listen(80,()=>console.log("server is started on : http://localhost:80"));
